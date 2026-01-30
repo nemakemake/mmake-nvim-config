5c0c05fab87e8f5f6d8f1b605d0b5070982c82e5
nemakemake <408629@niuitmo.ru>
2026-01-30 19:46:52 +0300

    now Snack plugin main

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -22,12 +22,12 @@ require('lazy').setup({
 
   -- modular approach: using `require 'path.name'` will
   -- include a plugin definition from file lua/path/name.lua
-
+  require 'kickstart.plugins.snacks',
   require 'kickstart.plugins.gitsigns',
 
   require 'kickstart.plugins.which-key',
 
-  require 'kickstart.plugins.telescope',
+  --require 'kickstart.plugins.telescope',
 
   require 'kickstart.plugins.lspconfig',
 
@@ -62,7 +62,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
